97402ef382fb0b29a4682ed464465547f3a0677b
Kasi Reddy <kasi.reddy@intel.com>
2016-06-27 17:39:36 +0100

    Cleaned up code

--- batduino/batduino.ino
+++ batduino/batduino.ino
@@ -1,22 +1,3 @@
-/**
- * \file MP3Shield_Library_Demo.ino
- *
- * \brief Example sketch of using the MP3Shield Arduino driver, demonstrating all methods and functions.
- * \remarks comments are implemented with Doxygen Markdown format
- *
- * \author Bill Porter
- * \author Michael P. Flaga
- *
- * This sketch listens for commands from a serial terminal (like the Serial
- * Monitor in the Arduino IDE). If it sees 1-9 it will try to play an MP3 file
- * named track00x.mp3 where x is a number from 1 to 9. For eaxmple, pressing
- * 2 will play 'track002.mp3'. A lowe case 's' will stop playing the mp3.
- * 'f' will play an MP3 by calling it by it's filename as opposed to a track
- * number.
- *
- * Sketch assumes you have MP3 files with filenames like "track001.mp3",
- * "track002.mp3", etc on an SD card loaded into the shield.
- */
 
 #include <SPI.h>
 
@@ -27,30 +8,15 @@
 //and the MP3 Shield Library
 #include <SFEMP3Shield.h>
 
+//sleep libraries
 #include <PinChangeInt.h>
 #include <avr/sleep.h>
 
-// Below is not needed if interrupt driven. Safe to remove if not using.
-#if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
-  #include <TimerOne.h>
-#elif defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer
-  #include <SimpleTimer.h>
-#endif
-
-/**
- * \brief Object instancing the SdFat library.
- *
- * principal object for handling all SdCard functions.
- */
 SdFat sd;
 const int buttonPin = 5;
 const int transPin = 10;
 int buttonState = 0;
-/**
- * \brief Object instancing the SFEMP3Shield library.
- *
- * principal object for handling all the attributes, members and functions for the library.
- */
+
 SFEMP3Shield MP3player;
 
 //------------------------------------------------------------------------------
@@ -58,113 +24,41 @@ SFEMP3Shield MP3player;
 void play()
 {
   digitalWrite(transPin, HIGH);
-  Serial.println("TEST");
-    buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
-  
-      Serial.println("Begin");
-      uint8_t result; // result code from some function as to be tested at later time.
-      MP3player.setVolume(2, 2); // commit new volume
-    #if USE_MULTIPLE_CARDS
-        sd.chvol(); // assign desired sdcard's volume.
-    #endif
-        //tell the MP3 Shield to play a track
-        result = MP3player.playTrack(1);
-    
-        //check result, see readme for error codes.
-        if(result != 0) {
-          Serial.print(F("Error code: "));
-          Serial.print(result);
-          Serial.println(F(" when trying to play track"));
-        } else {
-    
-          Serial.println(F("Playing:"));
-        }
-    
-        delay(2000);
-      }
+    MP3player.setVolume(2, 2); // commit new volume
+    MP3player.playTrack(1);
+    while (MP3player.isPlaying() != 0)
+      delay(50);
+  }
 }
 
-/**
- * \brief Setup the Arduino Chip's feature for our use.
- *
- * After Arduino's kernel has booted initialize basic features for this
- * application, such as Serial port and MP3player objects with .begin.
- * Along with displaying the Help Menu.
- *
- * \note returned Error codes are typically passed up from MP3player.
- * Whicn in turns creates and initializes the SdCard objects.
- *
- * \see
- * \ref Error_Codes
- */
-void setup() {
+void setup() { 
+  //Serial.begin(115200);
 
-  uint8_t result; //result code from some function as to be tested at later time.
-
-  Serial.begin(115200);
-
+  digitalWrite(transPin, LOW);
   pinMode(buttonPin, INPUT);
   PCintPort::attachInterrupt(buttonPin, play, RISING);
-  
-  //Initialize the SdCard.
-  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
-  // depending upon your SdCard environment, SPI_HAVE_SPEED may work better.
-  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
 
-  //Initialize the MP3 Player Shield
-  result = MP3player.begin();
-  //check result, see readme for error codes.
-  if(result != 0) {
-    Serial.print(F("Error code: "));
-    Serial.print(result);
-    Serial.println(F(" when trying to start MP3 player"));
-    if( result == 6 ) {
-      Serial.println(F("Warning: patch file not found, skipping.")); // can be removed for space, if needed.
-      Serial.println(F("Use the \"d\" command to verify SdCard can be read")); // can be removed for space, if needed.
-    }
-  }
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED))
+    sd.initErrorHalt();
+    
+  if(!sd.chdir("/")) 
+    sd.errorHalt("sd.chdir");
 
-#if defined(__BIOFEEDBACK_MEGA__) // or other reasons, of your choosing.
-  // Typically not used by most shields, hence commented out.
-  Serial.println(F("Applying ADMixer patch."));
-  if(MP3player.ADMixerLoad("admxster.053") == 0) {
-    Serial.println(F("Setting ADMixer Volume."));
-    MP3player.ADMixerVol(-3);
-  }
-#endif
+  MP3player.begin();
 
 }
 
-//------------------------------------------------------------------------------
-/**
- * \brief Main Loop the Arduino Chip
- *
- * This is called at the end of Arduino kernel's main loop before recycling.
- * And is where the user's serial input of bytes are read and analyzed by
- * parsed_menu.
- *
- * Additionally, if the means of refilling is not interrupt based then the
- * MP3player object is serviced with the availaible function.
- *
- * \note Actual examples of the libraries public functions are implemented in
- * the parse_menu() function.
- */
 void loop() {
- sleep();
+  digitalWrite(transPin, LOW);
+  sleep();
 }
 
 void sleep(){
-  Serial.println("SLEEPING");
-  digitalWrite(transPin, LOW);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
   sleep_cpu();
-  //RESTART FROM HERE
   sleep_disable();
-  
 }
-
-
-
 
