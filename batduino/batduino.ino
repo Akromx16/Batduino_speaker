3baf1cb1760a33c4d7292665f1dde1953a39c240
Adrian Moldovan <adrian.moldovan@intel.com>
2016-07-05 13:05:34 +0100

    Added functionality for the rotary switch

--- batduino/batduino.ino
+++ batduino/batduino.ino
@@ -17,27 +17,42 @@ const int buttonPin = 5;
 const int transPin = 10;
 int buttonState = 0;
 
+int analogue[6] = {14, 15, 16, 17, 18, 19};
+
 SFEMP3Shield MP3player;
 
 //------------------------------------------------------------------------------
 
 void play()
 {
+  int index = 0;
   digitalWrite(transPin, HIGH);
+  delay(100); // small delay to allow the circuitry to "wake up"
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
-    MP3player.setVolume(2, 2); // commit new volume
-    MP3player.playTrack(1);
-    while (MP3player.isPlaying() != 0)
-      delay(50);
+    // check the analogue inputs
+    for (index = 0; index < 6; index++) {
+      if (digitalRead(analogue[index]) == HIGH) {
+        MP3player.setVolume(2, 2); // commit new volume
+        MP3player.playTrack(index + 1);
+        while (MP3player.isPlaying() != 0)
+          delay(50);
+        break;
+      }
+    }
   }
 }
 
 void setup() { 
+  int index = 0;
   //Serial.begin(115200);
 
   digitalWrite(transPin, LOW);
   pinMode(buttonPin, INPUT);
+  
+  for (index = 0; index < 6; index++)
+    pinMode(analogue[index], INPUT);
+
   PCintPort::attachInterrupt(buttonPin, play, RISING);
 
   if(!sd.begin(SD_SEL, SPI_FULL_SPEED))
