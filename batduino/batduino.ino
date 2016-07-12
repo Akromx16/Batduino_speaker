4a324590cdde837b7ecbcd3f23755f96f5923c8b
Adrian Moldovan <adrian.moldovan@intel.com>
2016-07-12 18:08:51 +0100

    Added some error handling

--- batduino/batduino.ino
+++ batduino/batduino.ino
@@ -14,7 +14,10 @@
 
 SdFat sd;
 const int buttonPin = 5;
+const int frequencyPin = 4;
+const int errorLED = 13;
 int buttonState = 0;
+int freq = 0;
 
 int analogue[6] = {14, 15, 16, 17, 18, 19};
 
@@ -22,55 +25,73 @@ SFEMP3Shield MP3player;
 
 //------------------------------------------------------------------------------
 
-void play()
-{
-  int index = 0;
-  delay(100); // small delay to allow the circuitry to "wake up"
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    // check the analogue inputs
-    for (index = 0; index < 6; index++) {
-      if (digitalRead(analogue[index]) == HIGH) {
-        MP3player.setVolume(2, 2); // commit new volume
-        MP3player.playTrack(index + 1);
-        while (MP3player.isPlaying() != 0)
-          delay(50);
-        break;
-      }
-    }
-  }
-}
 
 void setup() { 
   int index = 0;
-  //Serial.begin(115200);
-
-  pinMode(buttonPin, INPUT);
+  uint8_t result; //result code from some function as to be tested at later time.
   
+  Serial.begin(115200);
+  //digitalWrite(frequencyPin, LOW);
+  pinMode(errorLED, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(frequencyPin, INPUT);
+  
+  digitalWrite(errorLED, HIGH);
+  delay(3000);
+
+  Serial.println("Begin");
+  //digitalWrite(errorLED, HIGH);
+
   for (index = 0; index < 6; index++)
     pinMode(analogue[index], INPUT);
 
 
-  if(!sd.begin(SD_SEL, SPI_FULL_SPEED))
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) {
     sd.initErrorHalt();
+    Serial.println("Error_1");
+    //digitalWrite(errorLED, HIGH);
     
-  if(!sd.chdir("/")) 
+  }
+    
+  if(!sd.chdir("/")) {
     sd.errorHalt("sd.chdir");
+    Serial.println("Error_2");
+    //digitalWrite(errorLED, HIGH);
+  }
+  delay(100);
+  result = MP3player.begin();
 
-  MP3player.begin();
-
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.println(result);
+  }
 }
 
 void loop() {
   int index = 0;
   // delay(100); // small delay to allow the circuitry to "wake up"
   buttonState = digitalRead(buttonPin);
+
+  
+  if (digitalRead(frequencyPin) == HIGH)
+    freq = 4;
+  else
+    freq = 0;
+
+
   if (buttonState == HIGH) {
+    Serial.println("Button_pressed");
     // check the analogue inputs
     for (index = 0; index < 6; index++) {
       if (digitalRead(analogue[index]) == HIGH) {
         MP3player.setVolume(2, 2); // commit new volume
-        MP3player.playTrack(index + 1);
+        uint8_t result = MP3player.playTrack(index + 1 + freq);
+        if(result != 0) {
+          Serial.print(F("Error code: "));
+          Serial.print(result);
+          Serial.println(F(" when trying to play track"));
+        }
+        
         while (MP3player.isPlaying() != 0)
           delay(50);
         break;
